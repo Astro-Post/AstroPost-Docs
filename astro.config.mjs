4bec596e5113aae1fb21891c5c2ee21aa0a8cec9
Airpioa <zylermillet5@gmail.com>
2024-07-03 17:12:44 -0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
       items: [
       // Each item here is one entry in the navigation menu.
       {
-        label: 'Example Guide',
+        label: 'Guide',
         link: '/guides/example/'
       }]
     }, {
