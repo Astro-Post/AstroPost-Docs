fb64f9d1af76771a1e02037aa5bc23f341741f89
Airpioa <zylermillet5@gmail.com>
2024-07-03 17:52:43 -0700

    PAges

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://airpioa.github.io',
+  base: 'AstroPost-Docs',
   integrations: [starlight({
     title: 'AstroPost Docs',
     social: {
